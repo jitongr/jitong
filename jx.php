245d6cbc3ac4ff806a04da930300d07415b5d2e1
jitong <2406677340@qq.com>
2018-06-16 14:59:18 +0800

    jx pic

--- jx.php
+++ jx.php
@@ -1,4 +1,6 @@
-
+<?php 
+//print_r($_GET);
+?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -7,39 +9,23 @@
 	<head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Cache &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<title>cruboy图集</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="description" content="Free HTML5 Template by FREEHTML5" />
-	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
-	
-
   
-
-  	<!-- Facebook and Twitter integration -->
-	<meta property="og:title" content=""/>
-	<meta property="og:image" content=""/>
-	<meta property="og:url" content=""/>
-	<meta property="og:site_name" content=""/>
-	<meta property="og:description" content=""/>
-	<meta name="twitter:title" content="" />
-	<meta name="twitter:image" content="" />
-	<meta name="twitter:url" content="" />
-	<meta name="twitter:card" content="" />
-
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<!-- <link rel="shortcut icon" href="favicon.ico"> -->
 	
-	<link rel="stylesheet" href="css/animate.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/icomoon.css">
+	<link rel="stylesheet" href="/asset/album/css/animate.css">
+	<link rel="stylesheet" href="/asset/album/css/bootstrap.css">
+	<link rel="stylesheet" href="/asset/album/css/icomoon.css">
 
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="/asset/album/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="/asset/album/css/owl.theme.default.min.css">
 
-	<link rel="stylesheet" href="css/style.css">
-	<script src="js/modernizr-2.6.2.min.js"></script>
+	<link rel="stylesheet" href="/asset/album/css/style.css">
+	<script src="/asset/album/js/modernizr-2.6.2.min.js"></script>
 	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
+	<script src="/asset/album/js/respond.min.js"></script>
 	<![endif]-->
 
 	</head>
@@ -54,54 +40,58 @@
 		<header id="fh5co-header" role="banner">
 			<div class="container">
 				<a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
-				<div id="fh5co-logo"><a href="index.html"><img src="images/logo.jpg" alt="Free HTML5 Website Template"></a></div>
+				<div id="fh5co-logo"><a href="/"><img src="/asset/album/images/cruboyalbum.png" ></a></div>
 				<nav id="fh5co-main-nav" role="navigation">
 					<ul>
-						<li><a href="about.html">About</a></li>
+						<li><a href="?s=1">个人</a></li>
 						<li class="has-sub">
 							<div class="drop-down-menu">
-								<a href="services.html">Services</a>
+								<a href="?s=2">制作</a>
 								<div class="dropdown-menu-wrap">
 									<ul>
-										<li><a href="#">Web Development</a></li>
-										<li><a href="#">Web Design</a></li>
-										<li><a href="#">Graphic Design</a></li>
-										<li><a href="#">Branding</a></li>
+										<li><a href="?s=21">实验室</a></li>
+										<li><a href="?s=22">经历</a></li>
+										<li><a href="?s=23">作品</a></li>
+										<li><a href="?s=24">机器人</a></li>
 									</ul>
 								</div>
 							</div>
 						</li>
-						<li><a href="portfolio.html">Portfolio</a></li>
+						<li><a href="?s=3">科技</a></li>
 						<li class="has-sub">
 							<div class="drop-down-menu">
-								<a href="#">Sidebars</a>
+								<a href="?s=5">默想</a>
 								<div class="dropdown-menu-wrap">
 									<ul>
-										<li><a href="right-sidebar.html">Right Sidebar</a></li>
-										<li><a href="left-sidebar.html">Left Sidebar</a></li>
+										<li><a href="?s=51">天主</a></li>
+										<li><a href="?s=52">赞美</a></li>
+										<li><a href="?s=52">祭献</a></li>
 										
 									</ul>
 								</div>
 							</div>
 						</li>
-						<li class="cta"><a href="contact.html">Contact</a></li>
+						<li class="cta"><a href="/">返回</a></li>
 					</ul>
 				</nav>
 			</div>
 		</header>
 		<!-- Header -->
-
+      <div class="fh5co-slider">
+			<div class="container">
+				<img src="/jty/17436291.jpg" alt="Free HTML5 Website Template" class="img-responsive">
+			</div>
+		</div>
 		<div class="fh5co-slider">
 			<div class="container">
 				<div class="owl-carousel owl-carousel-main">
-				  <div><img src="images/slider_1.jpg" alt="Free HTML5 Website Template"></div>
-				  <div><img src="images/slider_2.jpg" alt="Free HTML5 Website Template"></div>
-				  <div><img src="images/slider_3.jpg" alt="Free HTML5 Website Template"></div>
+				  <div><img src="/jty/childdeath.jpg" ></div>
+				  <div><img src="/jty/crucfon1.jpg" ></div>
+				   <div><img src="/jty/z3183.jpg" ></div>
 				</div>
 			</div>
 		</div>
-		<!-- Slider -->
-        <div class="copyrights">Collect from <a href="http://www.cssmoban.com/" >企业网站模板</a></div>
+	
 		
 		<div id="fh5co-intro" class="fh5co-section">
 			<div class="container">
@@ -136,7 +126,7 @@
 				</div>
 				<div class="row row-bottom-padded-sm">
 					<div class="col-md-6">
-						<p><img src="images/slider_1.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-bordered"></p>
+						<p><img src="/jty/z3183.jpg" alt="Free HTML5 Bootstrap Template" class="img-responsive img-bordered"></p>
 					</div>
 					<div class="col-md-6 padded-top">
 						<h3>Creative Title Here</h3>
@@ -153,21 +143,21 @@
 				<div class="row row-bottom-padded-sm">
 					<div class="col-md-4">
 						<div class="fh5co-service text-center">
-							<img src="images/slider_1.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+							<img src="/jty/cruboys.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
 							<h3>Graphic Design</h3>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ullam unde, totam harum laboriosam dolores.</p>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="fh5co-service text-center">
-							<img src="images/slider_2.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+							<img src="/jty/200508160000021man-480.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
 							<h3>Web Design</h3>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ullam unde, totam harum laboriosam dolores.</p>
 						</div>
 					</div>
 					<div class="col-md-4">
 						<div class="fh5co-service text-center">
-							<img src="images/slider_3.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
+							<img src="/jty/91727051.jpg" class="img-responsive img-bordered" alt="Free HTML5 Bootstrap Template by FreeHTML5.co">
 							<h3>Web Development</h3>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur ullam unde, totam harum laboriosam dolores.</p>
 						</div>
@@ -214,8 +204,8 @@
 				<div class="row">
 					<div class="col-md-12">
 						<div class="fh5co-copyright">
-							<p class="pull-left">&copy; 2016 Free Template. All Rights Reserved. </p>
-							<p class="pull-right">More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+							<p class="pull-left">&copy; cruboy. </p>
+							<p class="pull-right">cruboy</p>
 						</div>
 					</div>
 				</div>
@@ -228,11 +218,11 @@
 		<a href="#" class="js-gotop"><i class="icon-chevron-down"></i></a>
 	</div>
 	
-	<script src="js/jquery.min.js"></script>
-	<script src="js/jquery.easing.1.3.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/owl.carousel.min.js"></script>
-	<script src="js/main.js"></script>
+	<script src="/asset/album/js/jquery.min.js"></script>
+	<script src="/asset/album/js/jquery.easing.1.3.js"></script>
+	<script src="/asset/album/js/bootstrap.min.js"></script>
+	<script src="/asset/album/js/owl.carousel.min.js"></script>
+	<script src="/asset/album/js/main.js"></script>
 
 	</body>
 </html>
